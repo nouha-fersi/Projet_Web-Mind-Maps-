2ba436e623678ab3d0f2bbff5048e0ce5a5261fb
Bouachour-Med-Aziz <mohamedaziz.bouachour@ensi-uma.tn>
2022-04-03 20:38:59 +0100

    Commit
    Answers input and test

--- page3.js
+++ page3.js
@@ -1,21 +1,14 @@
-function checkAnswers(){
-    // The following is what I changed.
-    Student_answer = document.querySelector('[name="write here"]').value
-    Teacher_answer ="usa"
+var ele = document.getElementById("answer");
 
-    if (Student_answer.length === 0 || Teacher_answer.length === 0) {
-        alert("You must enter an answer to continue...");
-        return false;
-    }
-
-    if (Student_answer === Teacher_answer) {
-        alert("CONGRATULATIONS! Your answer is correct! You have advanced to the next level.");
-        document.body.innerHTML += '<button onclick="window.location.href = \'https://www.google.com\';">Next Riddle</button>'
-        //NOTE: here is where the button should be activated and click on it to advance to an hyperlink 
+ele.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    var answer = document.getElementById(ele.value.toLowerCase().trim());
+    if (answer == null) {
+      alert("Wrong");
+      ele.value = "";
     } else {
-        alert("Wrong answer, please, keep trying...");
-        //NOTE: here the button must be disabled
+      answer.classList.add("correctAns");
+      ele.value = "";
     }
-
-}
-
+  }
+});
