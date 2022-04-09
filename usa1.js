ff9eddb3d5cd1fe4f027dcbed10b4065dc2961a8
nouha fersi <nouha.fersi@ensi-uma.tn>
2022-04-09 18:25:16 +0200

    shake when  erro

--- usa1.js
+++ usa1.js
@@ -1,5 +1,4 @@
 var ele = document.getElementById("answer");
-
 ele.addEventListener("keyup", function (event) {
   if (event.keyCode === 13) {
     var answer = document.getElementById(ele.value.toLowerCase().trim());
