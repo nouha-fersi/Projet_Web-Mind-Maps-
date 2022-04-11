d453d135c4f5f21902ca6b954045b2574a0d6f15
Bouachour-Med-Aziz <mohamedaziz.bouachour@ensi-uma.tn>
2022-04-11 19:39:50 +0100

    Organisation des fichiers

---
+++ Code utile/flip card.js
@@ -0,0 +1,5 @@
+var x = document.getElementsByClassName(cube);
+x.onclick = function () {
+  //   -webkit-transform: rotateX(90deg);
+  //   rotateX(90deg);
+};
---
+++ Page 3/page3.js
@@ -0,0 +1,21 @@
+var ele = document.getElementById("answer");
+var i = 0;
+ele.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    var answer = document.getElementById(ele.value.toLowerCase().trim());
+    if (answer == null) {
+      alert("Wrong");
+      ele.value = "";
+    } else {
+      if (!answer.classList.contains("correctAns")) {
+        i++;
+        answer.classList.add("correctAns");
+      }
+      ele.value = "";
+    }
+  }
+});
+
+if (i == 6) {
+  
+}
