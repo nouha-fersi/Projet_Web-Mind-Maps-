815be621f6059554b6849753cb2816a860a68cf1
Bouachour-Med-Aziz <mohamedaziz.bouachour@ensi-uma.tn>
2022-04-14 11:00:39 +0100

    next button

--- Japan Levels/Level3.js
+++ Japan Levels/Level3.js
@@ -1,15 +1,20 @@
 var ele = document.getElementById("answer");
-
+var i = 0;
 ele.addEventListener("keyup", function (event) {
-    if (event.keyCode === 13) {
-      var answer = document.getElementById(ele.value.toLowerCase().trim());
-      if (answer == null) {
-        alert("Wrong");
-        ele.value = "";
-      } else {
+  if (event.keyCode === 13) {
+    var answer = document.getElementById(ele.value.toLowerCase().trim());
+    if (answer == null) {
+      alert("Wrong");
+      ele.value = "";
+    } else {
+      if (!answer.classList.contains("correctAns")) {
+        i++;
         answer.classList.add("correctAns");
-        ele.value = "";
+      }
+      ele.value = "";
+      if (i == 6) {
+        var n = (document.getElementById("next").style.visibility = "visible");
       }
     }
-  });
-  
+  }
+});
