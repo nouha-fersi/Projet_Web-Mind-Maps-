0d8addd825add7a8bd569cdf69daad0b75eb89f1
nouha fersi <nouha.fersi@ensi-uma.tn>
2022-04-15 22:54:57 +0200

    .

--- Code utile/tree.js
+++ Code utile/tree.js
@@ -1,94 +1,202 @@
-function init() {
-    // Since 2.2 you can also author concise templates with method chaining instead of GraphObject.make
-    // For details, see https://gojs.net/latest/intro/buildingObjects.html
-    const $ = go.GraphObject.make;  // for conciseness in defining templates
-    myDiagram =
-      $(go.Diagram, "myDiagramDiv",  // must be the ID or reference to div
-        {
-          "toolManager.hoverDelay": 100,  // 100 milliseconds instead of the default 850
-          allowCopy: false,
-          layout:  // create a TreeLayout for the family tree
-            $(go.TreeLayout,
-              { angle: 90, nodeSpacing: 10, layerSpacing: 40, layerStyle: go.TreeLayout.LayerUniform })
-        });
-
-        var bluegrad = '#90CAF9';
-        var pinkgrad = '#F48FB1';
-
-    // get tooltip text from the object's data
-    function tooltipTextConverter(person) {
-        var str = "";
-        return str;
-      }
-  
-    // define tooltips for nodes
-    var tooltiptemplate =
-      $("ToolTip",
-        { "Border.fill": "whitesmoke", "Border.stroke": "black" },
-        $(go.TextBlock,
-          {
-            font: "bold 8pt Helvetica, bold Arial, sans-serif",
-            wrap: go.TextBlock.WrapFit,
-            margin: 5
-          },
-          new go.Binding("text", "", tooltipTextConverter))
-      );
-
-    // define Converters to be used for Bindings
-    function genderBrushConverter(gender) {
-        if (gender === "M") return bluegrad;
-        if (gender === "F") return pinkgrad;
-        return "orange";
-      }
-
-    // replace the default Node template in the nodeTemplateMap
-    myDiagram.nodeTemplate =
-    $(go.Node, "Auto",
-    { deletable: false, toolTip: tooltiptemplate },
-        new go.Binding("text", "name"),
-        $(go.Shape, "Rectangle",
-          {
-            fill: "lightgray",
-            stroke: null, strokeWidth: 0,
-            stretch: go.GraphObject.Fill,
-            alignment: go.Spot.Center
-          },
-          new go.Binding("fill", "gender", genderBrushConverter)),
-          $(go.TextBlock,
-            {
-              font: "700 12px Droid Serif, sans-serif",
-              textAlign: "center",
-              margin: 10, maxSize: new go.Size(80, NaN)
-            },
-          new go.Binding("text", "name"))
-      );
-
-    // define the Link template
-    myDiagram.linkTemplate =
-    $(go.Link,  // the whole link panel
-    { routing: go.Link.Orthogonal, corner: 5, selectable: false },
-        $(go.Shape, { strokeWidth: 3, stroke: '#424242' }));  // the gray link shape
-
-    // here's the family data
-    var nodeDataArray = [
-      { key: 0, name: "George V"},
-      { key: 1, parent: 0, name: "USA"},
-      { key: 2, parent: 0, name: "JAPAN"},
-      { key: 3, parent: 0, name: "GERMANY"},
-      { key: 4, parent: 0, name: "ITALY" },
-      { key: 5, parent: 0, name: "SPAIN"},
-      { key: 6, parent: 0, name: "EGYPT"}
-    ];
-
-    // create the model for the family tree
-    myDiagram.model = new go.TreeModel(nodeDataArray);
-
-    document.getElementById('zoomToFit').addEventListener('click', () => myDiagram.commandHandler.zoomToFit());
-
-    document.getElementById('centerRoot').addEventListener('click', () => {
-      myDiagram.scale = 1;
-      myDiagram.scrollToRect(myDiagram.findNodeForKey(0).actualBounds);
-    });
-
+const uniqueId = (() => {
+  function* uniqueIdGenerator() {
+    let id = Date.now();
+    
+    while(true) {
+      yield id++;
+    }
   }
-  window.addEventListener('DOMContentLoaded', init);
+  
+  const gen = uniqueIdGenerator();
+  
+  return () => gen.next().value;
+})()
+
+class Tree {
+  #children = new Map();
+  #parent = null;
+  #id = uniqueId();
+  #name;
+  
+  constructor(name) {
+    if(!name || typeof name !== 'string' || !name.trim().length) {
+      throw new Error('Name must be a non-empty String');
+    }
+    
+    this.#name = name;
+  }
+
+  get name() {
+    return this.#name;
+  }
+  
+  set name(newName) {
+    if(!newName || typeof newName !== 'string' || !newName.trim().length) {
+      throw new Error('Cannot change name.Name must be a non-empty String');
+    }
+    
+    this.#name = newName;
+  }
+
+  get identifier() {
+    return this.#id;
+  }
+
+  get children() {
+    return Array.from(this.#children.values());
+  }
+
+  get parentNode() {
+    return this.#parent;
+  }
+
+  set parentNode(newParent) {
+    if(newParent !== this.parentNode && (newParent === null || newParent instanceof Tree)) {
+      if(this.#parent) {
+         this.#parent.removeChildNode(this);
+      }
+      
+      this.#parent = newParent;
+      
+      if(newParent) {
+        newParent.appendChildNode(this);
+      }
+    }
+  }
+
+  get childrenCount() {
+    return this.#children.size;
+  }
+
+  createChildNode(name) {
+    const newNode = new Tree(name);
+    this.#children.set(newNode.identifier, newNode);
+    newNode.parentNode = this;
+    
+    return newNode;
+  }
+
+  hasChildNode(needle) {
+    if(needle instanceof Tree) {
+      return this.#children.has(needle.identifier);
+    }
+    
+    for(let child of this.children) {
+      if(child.name === needle || this.identifier === needle) {
+        return true
+      }
+    }
+    
+    return false;
+  }
+
+  getChildNode(nameOrId) {
+    for(let child of this.children) {
+      if(child.name === nameOrId || this.identifier === nameOrId) {
+        return child;
+      }
+    }
+    
+    return null;
+  }
+
+  appendChildNode(node) {
+    if(!(node instanceof Tree) || this.hasChildNode(node)) return;
+    
+    if(node === this) throw new Error('Node cannot contain itself');
+    
+    let parent = this.parentNode;
+    while(parent !== null) {
+      if(parent === node) throw new Error('Node cannot contain one of its ancestors');
+      parent = parent.parentNode;
+    }
+    
+    this.#children.set(node.identifier, node);
+    node.parentNode = this;
+  }
+
+  removeChildNode(needle) {
+    if(!this.hasChildNode(needle)) return;
+    
+    let removedNode;
+    
+    if(needle instanceof Tree) {
+      this.#children.delete(needle.identifier);
+      removedNode = needle;
+    } else {
+      for(let child of this.children) {
+        if(child.name === needle || child.identifier === needle) {
+          this.#children.delete(child.identifier);
+          removedNode = child;
+          break;
+        }
+      }
+    }
+    
+    if(removedNode) {
+      removedNode.parentNode = null;
+    }
+  }
+
+  appendChildNode(node) {
+    if(!(node instanceof Tree) || this.hasChildNode(node)) return;
+    
+    if(node === this) throw new Error('Node cannot contain itself');
+    
+    let parent = this.parentNode;
+    while(parent !== null) {
+      if(parent === node) throw new Error('Node cannot contain one of its ancestors');
+      parent = parent.parentNode;
+    }
+    
+    this.#children.set(node.identifier, node);
+    node.parentNode = this;
+  }
+
+  #getTreeString = (node, spaceCount = 0) => {
+    let str = "\n";
+  
+    node.children.forEach((child) => {
+      str += `${" ".repeat(spaceCount)}${child.name}${this.#getTreeString(child, spaceCount + 2)}`
+    })
+
+    return str;
+  }
+
+  print() {
+    console.log(`\n${this.name}${this.#getTreeString(this, 2)}`);
+  }
+
+  traverse(cb) {
+    for(let child of this.children) {
+      if(cb(child) === true || child.traverse(cb) === true) {
+        return true;
+      }
+    }
+  }
+
+  findNodeByName(name) {
+    let foundNode = null;
+    
+    this.traverse(node => {
+      if(node.name === name) {
+        foundNode = node;
+        return true;
+      }
+    })
+    
+    return foundNode;
+  }
+
+  findAllNodesByName(name) {
+    const children = [];
+    
+    this.traverse(node => {
+      if(node.name === name ) {
+        children.push(node);
+      }
+    })
+    
+    return children;
+  }
+}
