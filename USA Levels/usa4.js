188d0bedf0137867f972e77aa01cb9bde8f81547
Bouachour-Med-Aziz <mohamedaziz.bouachour@ensi-uma.tn>
2022-04-21 11:05:43 +0100

    commit

---
+++ USA Levels/usa4.js
@@ -0,0 +1,34 @@
+var ele = document.getElementById("answer");
+ele.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    var answer = document.getElementById(ele.value.toLowerCase().trim());
+    if (answer == null) {
+      alert("Wrong");
+      ele.value = "";
+    } else {
+      answer.classList.add("correctAns");
+      ele.value = "";
+    }
+  }
+});
+// Get the modal
+var modal = document.getElementById('myModal');
+
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
